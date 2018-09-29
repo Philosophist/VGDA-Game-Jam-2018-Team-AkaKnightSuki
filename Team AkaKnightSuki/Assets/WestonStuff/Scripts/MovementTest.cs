2f2c59d30becc51438649f7b3adf0cc3b9784b87
Weston Elliott <Westonini101@gmail.com>
2018-09-29 11:19:10 -0700

    Weston's Version

---
+++ Team AkaKnightSuki/Assets/WestonStuff/Scripts/MovementTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour {
+    public float horizontalSpeed = 5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        transform.position = transform.position + new Vector3(horizontalInput * horizontalSpeed * Time.deltaTime, 0, 0);
+    }
+}
